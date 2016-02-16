22122481d78bf6a7c426ee565abd7d8db8f664cf
Eric Nelson <enelson@ancestry.com>
2016-02-16 13:13:18 -0700

    updates

--- elpa/idea-darkula-theme-20160121.1340/idea-darkula-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "idea-darkula-theme" "20160121.1340" "Color theme based on IntelliJ IDEA Darkula color theme" '((emacs "24.1")) :url "http://github.com/fourier/idea-darkula-theme" :keywords '("themes"))
--- elpa/jdee-20160130.1430/jdee-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "jdee" "20160130.1430" "Java Development Environment for Emacs"
-  '((emacs "24.3"))
-  :url "http://github.com/jdee-emacs/jdee" :keywords
-  '("java" "tools"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
